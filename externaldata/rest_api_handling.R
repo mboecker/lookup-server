19db4456ee24fa6f5c0dc6214af4646c20f45d79
Marvin Böcker <marvin.boecker@tu-dortmund.de>
2018-01-24 17:35:24 +0100

    Added simple version of the lookup. Not sure if this works in the container yet.

--- externaldata/rest_api_handling.R
+++ externaldata/rest_api_handling.R
@@ -3,21 +3,70 @@ library(RMySQL)
 mysql_username = "root"
 mysql_password = ""
 mysql_dbname = "openml"
-mysql_host = "localhost"
+mysql_host = "127.0.0.1"
 
 con <- dbConnect(MySQL(), user = mysql_username, password = mysql_password, dbname = mysql_dbname, host = mysql_host)
 
-source("sql_generator.R")
+source("/mysqldata/sql_generator.R")
 
-predict_point = function(task_id, parameters) {
+predict_point = function(impl_id = 6767, task_id = 3896, parameters = list(alpha = 5, eta = 3)) {
+  # Load parameter names for this algorithm
+  sql.exp = paste0("SELECT id, name FROM input WHERE implementation_id = ", impl_id)
+  query_results = dbGetQuery(con, sql.exp)
+  
+  if(dim(query_results)[1] == 0) {
+    stop("The algorithm you gave by implementation_id does not exist in this database.")
+  }
+  
+  # This contains the names for the parameters required by the chosen algorithm.
+  parameter_names = query_results$name
+  
+  # This contains the IDs for the parameters.
+  parameter_ids = query_results$id
+  
+  # This is the amount of parameters supplied.
+  n_parameters = length(parameters)
+  
+  # This will contain the input_ids for the supplied parameters in correct order.
+  parameter_ids_sorted = rep(0, n_parameters)
+  
+  # match function parameters onto input.name
+  for (i in 1:n_parameters) {
+    idx = which(parameter_names == names(parameters[i]))
+
+    if(length(idx) == 0) {
+      stop("This supplied parameter is not usable: ", names(parameters)[i])
+    }
+    else {
+      parameter_ids_sorted[i] = parameter_ids[idx]
+    }
+  }
+  
   # First, look in run for the task_id
   sql.exp = paste0("SELECT * FROM run WHERE uploader = ", uploader, " AND task_id = ", task_id);
-  run_data = dbGetQuery(mydb, sql.exp)
+  run_data = dbGetQuery(con, sql.exp)
   
-  # then, look in input_setting for the input_ids (for the names of parameters) and data on closest point (This can be loaded into memory)
-  # match function parameters onto input.name
-  sql.exp = generate_query(c(55774,55773,55790), c(5.2, 20.4,"Mersenne-Twister"))
+  if(dim(run_data)[1] == 0) {
+    stop("The task you requested has not yet been evaluated by the specified uploader.")
+  }
+  
+  # then, look in input_setting for the input_ids (for the names of parameters) and data on closest point.
+  # (This can be loaded into memory)
+  sql.exp = generate_query(task_id, parameter_ids_sorted, simplify2array(parameters))
   setup_data = dbGetQuery(con, sql.exp)
+  
+  if(dim(setup_data)[1] == 0) {
+    stop("No suitable points found in the database.")
+  }
+  
+  # For now, use the nearest point.
+  setup_id = setup_data$setup[1]
+  
+  # Now, we request performance data on the nearest point given by the database.
+  sql.exp = paste0("SELECT AVG(value) FROM evaluation WHERE source = (SELECT rid FROM run WHERE task_id = ", task_id, " AND setup = ", setup_id, ") AND function_id = 4");
+  performance_data = dbGetQuery(con, sql.exp)
+  
+  return(as.numeric(performance_data))
 }
 
 json_error <- function(err_msg, more=list()) {
@@ -28,7 +77,6 @@ json_error <- function(err_msg, more=list()) {
 lookup <- function(...) {
 	ls <- as.list(match.call())
 	ls[0:3] <- NULL
-	list(performance=ls)
 
 	notices = list()
 
