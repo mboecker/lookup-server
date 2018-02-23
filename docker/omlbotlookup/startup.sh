3b9a6a184dcc664d1c6e22c0c1a94253ac15d96f
Marvin Böcker <marvin.boecker@tu-dortmund.de>
2018-02-23 16:51:49 +0100

    docker now remembers sql loading state (closes #8)

--- docker/omlbotlookup/startup.sh
+++ docker/omlbotlookup/startup.sh
@@ -1,13 +1,17 @@
 # Start the database service.
 service mysql start
 
-# Create the necessary tables.
-echo "CREATE DATABASE IF NOT EXISTS openml;" | mysql
+if [ ! -f /root/already_loaded.lockfile ]; then
+	# Create the necessary tables.
+	echo "CREATE DATABASE IF NOT EXISTS openml;" | mysql
 
-echo "Loading data..."
-# Load the data from an external source.
-mysql openml < /mysqldata/database.sql
-echo "Data loading finished."
+	echo "Loading data..."
+	# Load the data from an external source.
+	mysql openml < /mysqldata/database.sql
+	echo "Data loading finished."
+
+	touch /root/already_loaded.lockfile
+fi
 
 # Start the REST server.
 cd /root/
