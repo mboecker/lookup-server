1029f376eb07eca7902b0238c76beb64b0863b93
Jakob Richter <code@jakob-r.de>
2018-03-12 11:16:09 +0100

    pkg stuff

--- docker/run-omlbotlookup.sh
+++ docker/run-omlbotlookup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -i --volume=$PWD/mysqldata:/mysqldata -p 8746:8000 --name=$USER-omlbotlookup omlbotlookup:$USER
+docker run -d --volume=$PWD/mysqldata:/mysqldata -p 8746:8000 --name=$USER-omlbotlookup omlbotlookup:$USER
---
+++ omlTuneBenchR/tests/testthat/00_starting_docker.R
@@ -0,0 +1,3 @@
+test_that("Docker container can be started", {
+  #
+})
