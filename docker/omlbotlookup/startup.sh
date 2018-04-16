1f2ef329cebd26148bc6d9f453b4fedd080131a2
Marvin Böcker <marvin.boecker@tu-dortmund.de>
2018-04-16 09:01:00 +0200

    Closes #27

--- docker/omlbotlookup/startup.sh
+++ docker/omlbotlookup/startup.sh
@@ -1,16 +1,27 @@
+#!/bin/bash
+
+# In a construct like "a | b", this causes "a | b" to fail, if a failed:
+set -o pipefail
+
 # Start the database service.
 service mysql start
 
 if [ ! -f /root/already_loaded.lockfile ]; then
-	# Create the necessary tables.
+	# Create an empty database.
 	echo "CREATE DATABASE IF NOT EXISTS openml;" | mysql
 
 	echo "Loading data..."
 	# Load the data from an external source.
 	mysql openml < /mysqldata/database.sql
-	echo "Data loading finished."
 
-	touch /root/already_loaded.lockfile
+	# Check if loading succeded.
+	if [ $? -eq 0 ]; then
+		echo "Data loading finished."
+		touch /root/already_loaded.lockfile
+	else
+		echo "Failed to load data!"
+		exit 1
+	fi
 fi
 
 # Start the REST server.
