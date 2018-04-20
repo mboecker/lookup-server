de87c0f3063186370c0b17c48e8076e6c72a7008
Jakob Richter <code@jakob-r.de>
2018-04-20 15:39:26 +0200

    try another way to start the service

--- docker/run-omlbotlookup.sh
+++ docker/run-omlbotlookup.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
-docker run -d --volume=$PWD/mysqldata:/mysqldata -p 8746:8746 --name=$USER-omlbotlookup omlbotlookup:$USER
+docker run -itd -p 8746:8746 --name=$USER-omlbotlookup omlbotlookup:$USER
+docker exec -i $USER-omlbotlookup service mysql start
+echo "CREATE DATABASE IF NOT EXISTS openml;" |  docker exec -i $USER-omlbotlookup mysql
+gzip -cd mysqldata/database.sql.gz | docker exec -i $USER-omlbotlookup mysql openml
+docker exec -d $USER-omlbotlookup bash -c "cd && /usr/bin/Rscript rest_server.R"
