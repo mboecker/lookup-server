030093e9520700b098135810d11d9342349c2d80
Jakob Richter <code@jakob-r.de>
2018-04-30 11:08:12 +0200

    diag in docker start

--- docker/run-omlbotlookup.sh
+++ docker/run-omlbotlookup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
+echo "Run docker..."
 docker run -itd -p 8746:8746 --name=$USER-omlbotlookup omlbotlookup:$USER
+echo "Start mysql on container..."
 docker exec -i $USER-omlbotlookup service mysql start
+echo "Diag file permissions..."
+docker exec -i $USER-omlbotlookup ls -al /var/log
+echo "Dump err.log"
+docker exec -i $USER-omlbotlookup cat /var/log/mysql/error.log
+echo "Create database on container..."
 echo "CREATE DATABASE IF NOT EXISTS openml;" |  docker exec -i $USER-omlbotlookup mysql
+echo "Dump snapshot to database..."
 gzip -cd mysqldata/database.sql.gz | docker exec -i $USER-omlbotlookup mysql openml
 docker exec -d $USER-omlbotlookup bash -c "cd && /usr/bin/Rscript rest_server.R"
