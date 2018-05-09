6b21c66be97f130a896bcf39fa62b30c23673f08
Jakob Richter <code@jakob-r.de>
2018-05-09 10:29:33 +0200

    Fix param api handling Fixes #43

--- docker/omlbotlookup/rest_api.R
+++ docker/omlbotlookup/rest_api.R
@@ -60,7 +60,10 @@ rest_estimate_performance = function(task = NULL, algo = NULL, parameters = NULL
   if (ncol(parameters) == 1) { #FIXME: Workaround for PH bug!
     parameter_list = lapply(parameters[, 1, drop = TRUE], function(x) setNames(list(x), names(parameters)))
   } else {
-    parameter_list = dfRowsToList(parameters, parameter_ranges[[algo]])  
+    parameter_list = dfRowsToList(
+      parameters[, getParamIds(parameter_ranges[[algo]]), drop = FALSE], 
+      parameter_ranges[[algo]],
+      ints.as.num = TRUE)  
   }
   parameter_status = sapply(parameter_list, function(x) is_parameter_list_ok(algo_name, x))
   parameter_status_ok = sapply(parameter_status, isTRUE)
