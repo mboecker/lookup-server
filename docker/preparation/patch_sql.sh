97adbd85781c89e65d09a4dfd01bef898c537d74
Marvin Böcker <marvin.boecker@tu-dortmund.de>
2018-08-26 17:19:49 +0200

    fix sql patch, only worked on our .sql before

--- docker/preparation/patch_sql.sh
+++ docker/preparation/patch_sql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 fn="ExpDB_SNAPSHOT.sql.gz"
 mv $fn $fn.bk
-pv $fn.bk | gzip -d | sed --expression="s/ENGINE/& ROW_FORMAT=DYNAMIC/" | gzip > $fn
+pv $fn.bk | gzip -d | sed --expression="s/ENGINE/ROW_FORMAT=DYNAMIC &/" | gzip > $fn
 rm $fn.bk
 
-#pv $fn.fullbk | gzip -d | sed --expression="s/ENGINE/& ROW_FORMAT=DYNAMIC/" | gzip > $fn
+#pv $fn.fullbk | gzip -d | sed --expression="s/ENGINE/ROW_FORMAT=DYNAMIC &/" | gzip > $fn
