4534235c0b9775a250dbb3e782e592cb899fb80b
Marvin Böcker <marvin.boecker@tu-dortmund.de>
2018-09-14 11:06:32 +0200

    use local copy of task meta data and fix preparation

--- docker/preparation/save_task_metadata.R
+++ docker/preparation/save_task_metadata.R
@@ -10,11 +10,13 @@ mysql_host = "127.0.0.1"
 con <- dbConnect(MySQL(), user = mysql_username, password = mysql_password, dbname = mysql_dbname_from, host = mysql_host)
 
 # Get raw data in wrong format
-sql.exp = sprintf("SELECT data, quality, value FROM %s.data_quality WHERE quality='NumberOfFeatures' OR quality='NumberOfInstances'", mysql_dbname_from)
+sql.exp = sprintf("SELECT data, quality, value FROM %s.data_quality WHERE quality='NumberOfFeatures' OR quality='NumberOfInstances' ORDER BY `data`", mysql_dbname_from)
 result = dbGetQuery(con, sql.exp)
 
 # Re-format data
-meta_data = data.frame(task.id = result$data, features = result[result$quality == "NumberOfFeatures",]$value, instances = result[result$quality == "NumberOfInstances",]$value)
+meta_data = data.frame(task_id = unique(result$data))
+meta_data$features = result[result$quality == "NumberOfFeatures", c("value")]
+meta_data$instances = result[result$quality == "NumberOfInstances", c("value")]
 
 # Save to file
 saveRDS(meta_data, file = "../omlbotlookup/task_metadata.Rds")
