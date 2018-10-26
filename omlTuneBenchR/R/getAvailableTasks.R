abe06e85acf1d6515afedef562041e30f43e2305
Jakob Richter <code@jakob-r.de>
2018-10-26 15:15:42 +0200

    allow tasks to be restricted to those from omlbot paper

--- omlTuneBenchR/R/getAvailableTasks.R
+++ omlTuneBenchR/R/getAvailableTasks.R
@@ -1,9 +1,15 @@
 #' Returns the task ids, which are saved in the database and can be queried.
 #'
 #' @return A vector of task ids.
+#' @param restrict [logical]
+#'   Restrict the tasks to those that appear in the following paper:
+#'   Automatic Exploration of Machine Learning Experiments on OpenML
+#'   Daniel K¨uhn, Philipp Probst, Janek Thomas, Bernd Bischl
+#'   https://arxiv.org/pdf/1806.10961.pdf
 #' @export
-getAvailableTasks = function() {
-  httr.res = httr::GET(paste0(omlTuneBenchR$connection, "/tasks"), httr::accept_json())
+getAvailableTasks = function(restrict = FALSE) {
+  assertFlag(restrict)
+  httr.res = httr::GET(paste0(omlTuneBenchR$connection, "/tasks"), query = list(restrict = restrict), httr::accept_json())
   res = httr::content(httr.res)
   if (!is.null(res$error)) {
     stop(res$error)
