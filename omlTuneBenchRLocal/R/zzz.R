ce1a97dcc06e84bcbf77f7aa858f7e1267a4ad15
Jakob Richter <code@jakob-r.de>
2018-11-21 10:28:38 +0100

    NAMESPACE + File handling

--- omlTuneBenchRLocal/R/zzz.R
+++ omlTuneBenchRLocal/R/zzz.R
@@ -8,6 +8,7 @@
 
 omlTuneBenchR = new.env()
 omlTuneBenchR$remote = "https://www.statistik.tu-dortmund.de/~richter/omltunebenchr"
+omlTuneBenchR$rdspath = path.expand("~/omlTuneBenchR/")
 omlTuneBenchR$cache_size = 512 #MB
 omlTuneBenchR$cache_table = data.table(learner_id = character(0), task_id = integer(0), last_accessed = .POSIXct(double(0)), data = list())
 
@@ -16,7 +17,6 @@ omlTuneBenchR$cache_table = data.table(learner_id = character(0), task_id = inte
   #omlTuneBenchR$task_metadata = task_metadata
 
   # set path to store rds files
-  omlTuneBenchR$rdspath = Sys.getenv("OML_TUNE_BENCH_RDSPATH") %??% "~/omlTuneBenchR/"
   config.file = "~/.config/omlTuneBenchRLocal.R"
   if (fs::file_exists(config.file)) {
     sys.source(config.file, envir = omlTuneBenchR)
